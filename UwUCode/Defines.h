90219a40ed8815df50716e57b6c39a62d82011bf
lenoob <78706766+lenoobwastaken@users.noreply.github.com>
2023-03-08 14:33:44 -0500

    Add files via upload

---
+++ UwUCode/Defines.h
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+#define mainUwU main
+#define cowwen ;
+#define vowd void
+#define stdOwO std
+#define pwint std::cout << 
+#define bwaket {
+#define cwosebwaket }
+#define retuwn return
+#define pawenthesis (
+#define cwosepawenthesis )
+#define integew int
+#define stwing std::string
+#define chaw char
+#define pointew *
+#define stwuct struct
+#define cwass class
+#define pubwic public
+#define fuwwcowwent :
+#define pwivate private
+#define namespawe namespace
+#define uswing using
+#define ihavestds std
+#define equwals =
+#define pwus +
+#define pwuspwus ++
+#define minuwsminuws --
+#define minuws -
+#define fow for
+#define autow auto
+#define endwine endl
+#define inpwut std::cin
+#define dwot .
+#define ifw if
---
+++ UwUCode/UwUCode.cpp
@@ -0,0 +1,19 @@
+
+
+#include "Defines.h"
+uswing namespawe ihavestds cowwen
+vowd mainUwU pawenthesis cwosepawenthesis
+bwaket
+    pwint "Hewwo Wowd OwO" cowwen
+    fow pawenthesis integew joe equwals 1 cowwen joe pwuspwus cowwen cwosepawenthesis
+    bwaket
+        ifw pawenthesis joe equwals 100 cwosepawenthesis
+        bwaket
+            pwint "Weached 100 UwU" cowwen
+        cwosebwaket
+
+        pwint joe << endwine cowwen
+        
+    cwosebwaket
+    
+cwosebwaket
